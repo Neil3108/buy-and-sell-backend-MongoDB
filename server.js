f488c1458237666e8acd963cb78edd4ba2109680
Neil3108 <prakharsharma3108@gmail.com>
2023-07-15 23:15:23 -0500

    Changed port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const path = require('path')
-const PORT = process.env.PORT || 3500
+const PORT = process.env.PORT || 8080
 const { logger, logEvents } = require('./middleware/logger')
 const errorHandler = require('./middleware/errorHandler')
 const cookieParser = require('cookie-parser')
