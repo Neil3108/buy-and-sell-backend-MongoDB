1eb61a74dad933d43254bb82debf8c27ad12192a
Neil3108 <prakharsharma3108@gmail.com>
2023-07-23 13:38:59 -0500

    Added AWS origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
-    'https://buy-and-sell-winnipeg.onrender.com/'
+    'https://buy-and-sell-winnipeg.onrender.com/',
+    'http://buy-and-sell-prakhar.s3-website.ca-central-1.amazonaws.com/listings'
     ]
 
 module.exports = allowedOrigins
